f25959f479dc2f7a4d237a26e0fe0b36ea032b0f
cetigerlily <celestigerlily@gmail.com>
2024-07-20 12:13:06 +0200

    Validate login and sign up

--- app/login/page.js
+++ app/login/page.js
@@ -1,52 +1,63 @@
 "use client";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Input } from "@nextui-org/react";
 import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 import { inputClassNames } from "../components/ClassNames";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
 
 export default function Page() {
-  // Start of tutorial
   const router = useRouter();
+
   async function handleLogin() {
-    console.log(`Logging in with: ${email}, ${password}`);
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: email, password: password }),
+      });
 
-    const res = await fetch("/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email: email, password: password }),
-    });
-
-    if (res.ok) {
-      router.push("/dashboard");
-    } else {
-      // TODO: Handle invalid credentials or other errors
+      if (res.ok) {
+        router.push("/dashboard");
+      } else {
+        console.log(res);
+        setEmail("");
+        setPassword("");
+      }
+    } catch (error) {
+      console.log(error);
+      setEmail("");
+      setPassword("");
     }
   }
 
-  // TODO: Check before form submit that password === confirmPassword
   async function handleSignUp() {
-    console.log(
-      `Signing up with: ${name}, ${email}, ${password}, ${confirmPassword}`
-    );
-    const res = await fetch("/api/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: name, email: email, password: password }),
-    });
-    // const res = await createUser(newUser);  // NOTE: Weirdly doesn't work when I use this
-    if (res.ok) {
-      console.log("Created account successfully");
-      setIsLogin(true);
-      setName("");
-      setEmail(email);
-      setPassword("");
-      setConfirmPassword("");
+    try {
+      const res = await fetch("/api/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: name, email: email, password: password }),
+      });
+      if (res.ok) {
+        console.log("Created account successfully");
+        setIsLogin(true);
+        setName("");
+        setEmail(email);
+        setPassword("");
+        setConfirmPassword("");
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
   const [isLogin, setIsLogin] = useState(true);
   const toggleIsLogin = () => {
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
     setIsLogin(!isLogin);
   };
 
@@ -72,57 +83,82 @@ export default function Page() {
     setIsCPWVisible(!isCPWVisible);
   };
 
-  function handleLogin() {
-    // TODO: Handle incorrect email/password
-    console.log(`Logging in with: ${email}, ${password}`);
-  }
-
-  async function handleSignUp() {
-    if (password != confirmPassword) {
-      // TODO: Indicate CPW error message as not matching password
+  const validateEmail = (email) => {
+    const emailSchema = z.string().email();
+    try {
+      emailSchema.parse(email);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
+  };
+  const isInvalidEmail = useMemo(() => {
+    if (email != "") {
+      return !validateEmail(email);
+    }
+    return false;
+  });
 
-    console.log(
-      `Signing up with: ${name}, ${email}, ${password}, ${confirmPassword}`
-    );
+  const validateName = (name) => {
+    const nameSchema = z.string().refine((str) => /^[a-zA-Z]+$/.test(str));
+    try {
+      nameSchema.parse(name);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+  const isInvalidName = useMemo(() => {
+    if (name != "") {
+      return !validateName(name);
+    }
+    return false;
+  });
 
-    const newUser = {
-      name: name,
-      email: email,
-      password: password,
-    };
-    const res = await fetch("/api/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newUser),
-    });
-    console.log(res);
-  }
+  const validatePW = (password) => {
+    const PWSchema = z.string().min(5);
+    try {
+      PWSchema.parse(password);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+  const isInvalidPW = useMemo(() => {
+    if (password != "") {
+      return !validatePW(password);
+    }
+    return false;
+  });
+  const isInvalidCPW = useMemo(() => {
+    if (confirmPassword != "") {
+      return password != confirmPassword;
+    }
+    return false;
+  });
 
   return (
-    // TODO: Make text unable to be highlighted
     <div className="w-screen h-screen flex flex-row justify-center p-8 bg-a-pink/80">
       <div className="w-[400px] flex flex-col justify-center overflow-scroll p-5 gap-y-5 rounded-[20px] border border-a-black/10 bg-white">
         <div className="w-full flex flex-col items-center gap-y-5">
-          <p className="font-display font-bold text-2xl text-[#1F4776] text-center">
+          <p className="font-display font-bold text-2xl text-a-navy text-center">
             NUS AEROBICS
           </p>
-          {/* <p className="font-display font-bold text-2xl text-a-navy">
-            NUS AEROBICS
-          </p> */}
           <p className="font-poppins font-bold text-lg text-a-black">
             {message}
           </p>
         </div>
-
         <div className="w-full flex flex-col gap-y-1">
           {!isLogin ? (
             <Input
               label="Full name"
               value={name}
               onValueChange={setName}
-              // isInvalid={isInvalidName}
-              // errorMessage="Please enter a valid name"
+              isInvalid={isInvalidName}
+              errorMessage="Please enter a valid name"
               isRequired
               variant="bordered"
               size="sm"
@@ -135,8 +171,8 @@ export default function Page() {
             label="Email"
             value={email}
             onValueChange={setEmail}
-            // isInvalid={isInvalidEmail}
-            // errorMessage="Please enter a valid email"
+            isInvalid={isInvalidEmail}
+            errorMessage="Please enter a valid email"
             isRequired
             variant="bordered"
             size="sm"
@@ -160,8 +196,8 @@ export default function Page() {
                 )}
               </button>
             }
-            // isInvalid={isInvalidPW}
-            // errorMessage="Password should be at least 5 characters"
+            isInvalid={isInvalidPW}
+            errorMessage="Password should be at least 5 characters"
             isRequired
             variant="bordered"
             size="sm"
@@ -186,8 +222,8 @@ export default function Page() {
                   )}
                 </button>
               }
-              // isInvalid={isInvalidCPW}
-              // errorMessage="Passwords do not match"
+              isInvalid={isInvalidCPW}
+              errorMessage="Passwords do not match"
               isRequired
               variant="bordered"
               size="sm"
@@ -196,6 +232,7 @@ export default function Page() {
           ) : (
             <></>
           )}
+          {/* TODO: Implement handling forgot password */}
           {isLogin ? (
             <button className="text-end text-xs text-a-navy font-bold underline">
               Forgot password?
@@ -204,10 +241,9 @@ export default function Page() {
             <></>
           )}
         </div>
-
         <div className="w-full flex flex-col gap-y-1">
           <button
-            className="rounded-[30px] px-[20px] py-[10px] bg-[#1F4776] text-sm text-white"
+            className="rounded-[30px] px-[20px] py-[10px] bg-a-navy text-sm text-white"
             onClick={isLogin ? handleLogin : handleSignUp}
           >
             {button}
