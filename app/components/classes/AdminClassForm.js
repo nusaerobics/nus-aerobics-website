35bfa43a22d80fbc4c184877ef9e407c18e11008
cetigerlily <celestigerlily@gmail.com>
2024-07-23 15:18:49 +0200

    Handle date and time with timezones

--- app/components/classes/AdminClassForm.js
+++ app/components/classes/AdminClassForm.js
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { SectionTitle } from "../Titles";
 import { z } from "zod";
 import { format } from "date-fns";
+import { fromZonedTime, toZonedTime } from "date-fns-tz";
 
 export default function AdminClassForm({
   isCreate, // True or False, to determine whether to POST or UPDATE Class
@@ -17,10 +18,12 @@ export default function AdminClassForm({
 }) {
   const [name, setName] = useState(selectedClass.name);
   const [date, setDate] = useState(
-    selectedClass.date != "" ? format(selectedClass.date, "yyy-MM-dd") : ""
+    selectedClass.date != ""
+      ? format(selectedClass.date, "yyy-MM-dd")
+      : "YYYY-MM-DD"
   );
   const [time, setTime] = useState(
-    selectedClass.date != "" ? format(selectedClass.date, "HH:mm") : ""
+    selectedClass.date != "" ? format(selectedClass.date, "HH:mm") : "HH:mm"
   );
   const [description, setDescription] = useState(selectedClass.description);
   const [isOpenBooking, setIsOpenBooking] = useState(
@@ -28,13 +31,16 @@ export default function AdminClassForm({
   );
 
   async function createClass() {
-    // TODO: Fix timing issue since 19:00 becomes 18:00 - need to add timezone to inputs
     // TODO: Validate date and time input formats
     try {
+      const concatDate = `${date} ${time}:00`;
+      const utcDate = fromZonedTime(concatDate, "Asia/Singapore");
+      const newDate = toZonedTime(utcDate, "Asia/Singapore");
+
       const newClass = {
         name: name,
         description: description,
-        date: `${date} ${time}:00`,
+        date: newDate,
         status: isOpenBooking ? "open" : "closed",
       };
       const res = await fetch("/api/classes", {
@@ -52,11 +58,15 @@ export default function AdminClassForm({
 
   async function saveEdit() {
     try {
+      const concatDate = `${date} ${time}:00`;
+      const utcDate = fromZonedTime(concatDate, "Asia/Singapore");
+      const updateDate = toZonedTime(utcDate, "Asia/Singapore");
+
       const updatedClass = {
         id: selectedClass.id,
         name: name,
         description: description,
-        date: `${date} ${time}:00`,
+        date: updateDate,
         maxCapacity: selectedClass.maxCapacity,
         bookedCapacity: selectedClass.bookedCapacity,
         status: isOpenBooking ? "open" : "closed",
