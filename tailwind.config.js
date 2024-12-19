0ee5184337177182d32a5bc352e40c0f68d344e7
cetigerlily <celestigerlily@gmail.com>
2024-12-19 14:57:49 +0100

    Revert "Adjust for build"
    This reverts commit a5811e26602e5c204d86c96d13bd38ba9ffa35b0.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import { nextui } from "@nextui-org/react";
 
-export default {
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
