ea9f7eec3d7f9f006856226bcc1d0b3edd1f876b
cetigerlily <celestigerlily@gmail.com>
2024-12-19 14:24:29 +0100

    Adjust for build

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
 // TODO: Remove comment after build bugfix resolved
 
-import Layout from "../components/layout";
+// import Layout from "../components/layout";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
   return (
-      <Layout>
-        <Component {...pageProps} />;
-      </Layout>
+      // <Layout>
+        <Component {...pageProps} />
+      // </Layout>
   );
 }
