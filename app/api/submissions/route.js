eb47d65158223eeddd76451180c938d586f946f3
cetigerlily <celestigerlily@gmail.com>
2024-12-22 18:27:15 +0100

    Work on routes for handling submissions

--- app/api/submissions/route.js
+++ app/api/submissions/route.js
@@ -1,24 +1,14 @@
 import {NextResponse} from "next/server";
+import bcrypt from "bcrypt";
 
 const db = require("../../config/sequelize");
 const Submission = db.submissions;
+const User = db.users;
+const Transaction = db.transactions;
 
 // getSubmissions
-export async function GET(request) {
+export async function GET() {
   try {
-    const url = new URL(request.url);
-    const searchParams = new URLSearchParams(url.searchParams);
-
-    // getSubmissionBySubmissionId
-    if (searchParams.get("id") !== undefined) {
-      const submissionId = searchParams.get("id");
-      const submission = await Submission.findOne({ where: { submissionId: submissionId } });
-      if (!submission) {
-        throw new Error(`Submission ${submissionId} does not exist`);
-      }
-      return NextResponse.json(submission, { status: 200 });
-    }
-
     const submissions = await Submission.findAll();
     return NextResponse.json(submissions, { status: 200 });
   } catch (error) {
@@ -29,17 +19,76 @@ export async function GET(request) {
 
 // createSubmission
 export async function POST(request) {
+  const t = await db.sequelize.transaction();
   try {
     const body = await request.json();
-    const { submissionId, email } = body;
-    const data = await Submission.create({
-      submissionId: submissionId,
-      email: email,
-    })
-    return NextResponse.json(data, { status: 200 });
+    const { submissionId, name, email, totalCredits } = body;
+
+    // 1. if submission id not in db, continue. else, don't need to continue.
+    const existingSubmission = await Submission.findOne(
+      { where: { submissionId: submissionId } },
+      { t },
+    );
+    if (existingSubmission) {
+      throw new Error(`Submission ${submissionId} already exists`);
+    }
+
+    // 2. add submission to db with pending status.
+    const newSubmission = await Submission.create(
+      {
+        submissionId: submissionId,
+        email: email,
+      },
+      { transaction: t },
+    );
+
+    // 3. if user already exists, find user. else, create new user with email.
+    let user;
+    const existingUser = await User.findOne(
+      { where: { email: email } },
+      { t },
+    );
+    if (!existingUser) {
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(email, saltRounds);
+      user = await User.create(
+        {
+          name: name,
+          email: email,
+          password: hashedPassword,
+          },
+          { transaction: t },
+      );
+    } else {
+      user = existingUser;
+    }
+
+    // 4. update user's balance with total amount of credits.
+    const newBalance = parseInt(user.balance) + parseInt(totalCredits);
+    await User.update(
+      { balance: newBalance },
+      {
+        where: { id: user.id },
+        transaction: t,
+      });
+
+    // 5. update submission status.
+    await Submission.update({ status: "credited" }, { where: { id: newSubmission.id }, transaction: t });
+
+    // 6. create transaction.
+    await Transaction.create({
+      userId: user.id,
+      amount: totalCredits,
+      type: "deposit",
+      description: `Deposited ${totalCredits} credit(s)`
+    }, { transaction: t });
+
+    await t.commit();
+    return NextResponse.json(`Handled submission ${submissionId} for user ${user.id}`, { status: 200 });
   } catch (error) {
     console.log(error);
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    await t.rollback();
+    return NextResponse.json({ error: `Error handling submission: ${error.message}` }, { status: 500 });
   }
 }
 
